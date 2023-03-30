f4c1c1fd62f2e121d3073424eddcaf4a8baff34e
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-30 12:57:10 -0700

    Add query to pull all people from apollo using useQuery, render each person to the screen

--- react-graphql/src/App.js
+++ react-graphql/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      <h2>Phone Book</h2>
+      {persons.map((person) => (
+        <div key={person.name}>
+          {person.name} : {person.phone}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 function App() {
+  const result = useQuery(ALL_PERSONS);
+
+  if (result.loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Persons persons={result.data.allPersons} />
     </div>
   );
 }
