c0255589efd5012f2c9889f18e46bb82be5b19ef
Jacob Templeton <jacob.templeton@gmail.com>
2023-04-04 18:58:35 -0700

    Add LoginForm component, add LOGIN query that submits username and password, sets token in state if username and password is valid

--- react-graphql/src/App.js
+++ react-graphql/src/App.js
@@ -1,27 +1,52 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import Persons from "./components/Persons";
+import { ALL_PERSONS } from "./queries";
+import PersonForm from "./components/PersonForm";
+import { useState } from "react";
+import PhoneForm from "./components/PhoneForm";
+import LoginForm from "./components/LoginForm";
 
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
   }
-`;
+  return <div style={{ color: "red" }}>{errorMessage}</div>;
+};
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [token, setToken] = useState(null);
+
   const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
     return <div>Loading...</div>;
   }
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
+      <Notify errorMessage={errorMessage} />
+      <PersonForm setError={notify} />
       <Persons persons={result.data.allPersons} />
+      <PhoneForm setError={notify} />
     </div>
   );
 }
--- react-graphql/src/components/Persons.js
+++ react-graphql/src/components/Persons.js
@@ -1,19 +1,6 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { useState } from "react";
-
-const FIND_PERSON = gql`
-  query findPersonByName($nameToSearch: String!) {
-    findPerson(name: $nameToSearch) {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
-    }
-  }
-`;
+import { FIND_PERSON } from "../queries";
 
 const Person = ({ person, onClose }) => {
   return (
@@ -30,15 +17,23 @@ const Person = ({ person, onClose }) => {
 
 const Persons = ({ persons }) => {
   const [nameToSearch, setNameToSearch] = useState(null);
+
+  //   giving nameToSearch a value causes the component to re-render itself. On render
+  //   the query FIND_PERSON that fetches the detailed information of a user is executed
+  //   if the variable nameToSearch has a value
   const result = useQuery(FIND_PERSON, {
     variables: { nameToSearch },
     skip: !nameToSearch,
   });
 
+  //   If the state nameToSearch has a value and the query result is ready,
+  //   the component Person renders the detailed info of a person
   if (nameToSearch && result.data) {
     return (
       <Person
         person={result.data.findPerson}
+        // When a user wants to return to the persons list,
+        // the nameToSearch state is set to null.
         onClose={() => setNameToSearch(null)}
       />
     );
@@ -49,6 +44,8 @@ const Persons = ({ persons }) => {
       {persons.map((person) => (
         <div key={person.name}>
           {person.name} : {person.phone}
+          {/* When the button show address of a person is pressed, 
+          the name of the person is set to state nameToSearch */}
           <button onClick={() => setNameToSearch(person.name)}>
             show address
           </button>
